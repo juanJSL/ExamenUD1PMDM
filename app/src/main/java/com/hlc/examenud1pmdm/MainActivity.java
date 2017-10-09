accb55a3cfe1aea5e8bf2a801c17fe927c279e94
Unknown <jjolula88@gmail.com>
2017-10-09 14:24:46 +0200

    Comentado, vaciarEditText añadido

--- app/src/main/java/com/hlc/examenud1pmdm/MainActivity.java
+++ app/src/main/java/com/hlc/examenud1pmdm/MainActivity.java
@@ -6,12 +6,25 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText viewNumero;
-    private TextView viewResultado;
-    private int n;
-    private int result;
+/**
+ *@author Juan Javier Simón Lucas
+ *
+ * Esta aplicación permite que el usuario introduzca un número y calcular
+ * el factorial de este al pulsar un boton.
+ */
 
+public class MainActivity extends AppCompatActivity {
+    //-----VARIABLES-----
+    private EditText viewNumero;//EditText de aqui cogeremos el nº introducido por el usuario
+    private TextView viewResultado;//TextView aqui mostraremos el resultado despues de calcular el factorial
+    private int n;//Cogera el valor tecleado por el usuario
+    private int result;//variable que almacenara el resultado de calcular el factorial
+
+    /**
+     * Nos permite crear la activity principal especificada en el xml activity_main
+     * @param savedInstanceState //Nos permite crear la activity y recuperar su estado si no se a eliminado
+     *                          si es la primera vez que se ejecuta su valor sera null
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +38,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //Calcula el factorial de un numero
+    /**
+     * Metodo que vacia el editText al hacer click en el
+     */
+    public void vaciarEditText(View view){
+        viewNumero.setText("");
+    }
+
+    /**
+     * Metodo que calcula el factorial de un numero
+     */
     public void factorial (View view) {
         n = Integer.parseInt(String.valueOf(viewNumero.getText()));
         result=1;
